2875a47f8bbeb6f7ed9abfc5de8342d3a06d107c
ilyass atlassi <atlassiilyass@gmail.com>
2022-11-24 09:12:35 +0100

    Add the change suggest by the reviewer

--- script/main.js
+++ script/main.js
@@ -4,20 +4,14 @@ const cancel = document.querySelector('.delete');
 const links = document.querySelector('.links');
 const menu = document.querySelector('.menu-items');
 
-hamburger.addEventListener('click', () => {
+const activeToggle = () => {
   menu.classList.toggle('active');
   links.classList.toggle('active');
   cancel.classList.toggle('active');
-});
+};
 
-document.querySelectorAll('.links').forEach((n) => n.addEventListener('click', () => {
-  menu.classList.toggle('active');
-  links.classList.toggle('active');
-  cancel.classList.toggle('active');
-}));
+hamburger.addEventListener('click', activeToggle);
 
-cancel.addEventListener('click', () => {
-  menu.classList.toggle('active');
-  links.classList.toggle('active');
-  cancel.classList.toggle('active');
-});
+document.querySelectorAll('.links').forEach((n) => n.addEventListener('click', activeToggle));
+
+cancel.addEventListener('click', activeToggle);
