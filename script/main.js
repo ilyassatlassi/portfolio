cde3fe405927805b0040627a91c9849610395db3
ilyass atlassi <atlassiilyass@gmail.com>
2022-11-25 12:11:10 +0100

    Fix the form script

--- script/main.js
+++ script/main.js
@@ -5,9 +5,10 @@ const links = document.querySelector('.links');
 const menu = document.querySelector('.menu-items');
 // const button = document.querySelector('.btn-project');
 // const main = document.querySelector('.project');
-// const email = document.querySelector('.email');
-// const form = document.querySelector('.form');
-// const Warning = document.querySelector('.warning');
+const email = document.querySelector('.email');
+const form = document.querySelector('.my-form');
+const Warning = document.querySelector('.warning');
+const valid = document.querySelector('.button-contact');
 
 const activeToggle = () => {
   menu.classList.toggle('active');
@@ -21,12 +22,13 @@ document.querySelectorAll('.links').forEach((n) => n.addEventListener('click', a
 
 cancel.addEventListener('click', activeToggle);
 
+// form
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-
-  if (email.value === email.value.toLowerCase()) {
-    form.submit();
+  if (email.value !== email.value.toLowerCase()) {
+    Warning.innerHTML = 'Please the content of the email should be in lower case.';
   } else {
-    Warning.innerHTML = 'Please the content of the email field has to be in lower case.';
+    Warning.innerHTML = '';
+    form.submit();
   }
 });
