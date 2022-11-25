1da50858254d00130b3a82a325bb412dc24071e8
ilyass atlassi <atlassiilyass@gmail.com>
2022-11-25 17:32:19 +0100

    Change the button name

--- script/main.js
+++ script/main.js
@@ -3,14 +3,11 @@ const hamburger = document.querySelector('.toolbar');
 const cancel = document.querySelector('.delete');
 const links = document.querySelector('.links');
 const menu = document.querySelector('.menu-items');
-// const button = document.querySelector('.btn-project');
-// const main = document.querySelector('.project');
 const email = document.querySelector('.email');
 const form = document.querySelector('.my-form');
 const Warning = document.querySelector('.warning');
-const textarea = document.querySelector('.texarea');
+const textarea = document.querySelector('.textarea');
 const nameForm = document.querySelector('.name-form');
-// const valid = document.querySelector('.button-contact');
 
 const activeToggle = () => {
   menu.classList.toggle('active');
@@ -56,6 +53,11 @@ form.addEventListener('submit', (e) => {
     Warning.innerHTML = 'Please the content of the email should be in lower case.';
   } else {
     Warning.innerHTML = '';
+    persistData();
     form.submit();
   }
 });
+
+nameForm.addEventListener('change', getData);
+email.addEventListener('change', getData);
+textarea.addEventListener('change', getData);
